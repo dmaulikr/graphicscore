37046de8e69abac2fa2574ae1dfe7db2c2ca8a25
Zebedee Pedersen <zeb@zebpedersen.com>
2012-02-07 11:19:44 +0000

    Added audio effects methods

--- graphicscore/CAController.mm
+++ graphicscore/CAController.mm
@@ -34,10 +34,17 @@ float*	z_pitches	= new float [480];
 
 //	MAXI OBJECT DECLARATION
 
+maxiOsc		metro;
+int			metroCount;
+double		metroOut;
+double		metroSpeed;
+
 maxiSample	sample;
 double		sampleSpeed;
 
-bool	wTrig, xTrig, yTrig, zTrig;
+maxiOsc		testOsc;
+double		testFreq	= 0.0f;
+double		testVol		= 0.0f;
 
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
@@ -57,10 +64,16 @@ inline void initLocalVariables	(void)	{
 
 inline void maxiSetup		(void)	{
 	initLocalVariables();
-	wTrig = xTrig = yTrig = zTrig = true;
 	
 	sampleSpeed = 0;
 	sample.load([[[NSBundle mainBundle] pathForResource:@"voice_one" ofType:@"wav"]cStringUsingEncoding:NSUTF8StringEncoding]);
+	
+	testVol	 = 1.0f;
+	testFreq = 0.0f;
+	
+	metroSpeed	= 480.0f/5.0f;
+	metroOut	= 0.0f;
+	metroCount	= 0;
 }
 
 ////////////////////////////////////////////////////////////
@@ -70,54 +83,158 @@ float	trigger_index = 0;
 float	triggerIncrement = 0.0018;
 
 inline void readTriggers	(int i)	{	
-	if (w_triggers[i]>=0.01f && wTrig)	{
-		sampleSpeed = 1.5;//+(0.5*(80/w_pitches[i]));
+	if (w_triggers[i]>=0.01f)	{
+		sampleSpeed = 2.0f+(0.25f*w_pitches[i]);
 		sample.trigger();
-		wTrig = false;
+		testFreq	= 880.0f + (110.0f*w_pitches[i]);
+		testVol		= 1.0f;
 	}
-	else if (!w_triggers[i]>=0.01f)
-		wTrig = true;
-	
-	if (x_triggers[i]>=0.01f && xTrig)	{
-		sampleSpeed = 1.0;//+(0.5*(80/x_pitches[i]));
-		sample.trigger();
-		xTrig = false;	
-	}
-	else if (!x_triggers[i]>=0.01f)
-		xTrig = true;
 
-	if (y_triggers[i]>=0.01f && yTrig)	{
-		sampleSpeed = 0.5;//+(0.5*(80/y_pitches[i]));
+	if (x_triggers[i]>=0.01f)	{
+		sampleSpeed = 1.0f+(0.125*x_pitches[i]);
 		sample.trigger();
-		yTrig = false;
+		testFreq	= 440.0f + (55.0f*x_pitches[i]);
+		testVol = 1.0f;		
 	}
-	else if (!y_triggers[i]>=0.01f)
-		yTrig = true;
+
+	if (y_triggers[i]>=0.01f)	{
+		sampleSpeed = 0.5+(0.0625*y_pitches[i]);
+		sample.trigger();
+		testFreq	= 220.0f + (27.5f*y_pitches[i]);
+		testVol = 1.0f;		
+	}
 	
-	if (z_triggers[i]>=0.01f && zTrig)	{
-		sampleSpeed = 0.2;//+(0.5*z_pitches[i]);
+	if (z_triggers[i]>=0.01f)	{
+		sampleSpeed = 0.25+(0.03125*z_pitches[i]);
 		sample.trigger();
-		zTrig = false;
+		testFreq	= 110.0f + (13.75f*z_pitches[i]);
+		testVol = 1.0f;
 	}
-	else if (!z_triggers[i]>=0.01f)
-		zTrig = true;
 }
 
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
 
+maxiFilter high_pass;
+maxiFilter low_pass;
+
+maxiSample samplePlayer;
+
+maxiDelayline delay_one;
+maxiDelayline delay_two;
+maxiDelayline delay_three;
+maxiDelayline delay_four;
+
+maxiDelayline	flanger;
+maxiOsc			flangerCTL;
+
+maxiOsc am;
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+inline double	distortion		(double input, double amount)	{
+	input*=floor(24*amount);
+	input = input > 1 ? 1	: input;
+	input = input < -1 ? -1 : input;
+	return input * 0.6;	
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+int		crushCounter = 0;
+double	previousInput = 0.0f;
+inline double	bitcrusher		(double input, double amount)	{
+	if (crushCounter>=floor(16*amount))
+		crushCounter = 0;
+	else	input = previousInput;
+	crushCounter++;	
+	previousInput = input;
+	return input;	
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+inline double	lowpass		(double input, double res)	{
+	input = low_pass.lores(input, 100+(2000*res), 0.95-res);	
+	return input;	
+}
+
+//	HIPASS
+inline double	highpass	(double input, double res)	{
+	input = high_pass.hires(input, 100+(3000*res), 0.95-res);
+	return input;	
+}
+
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+double	i;
+inline double	delay	(double input, double amount, double mix)	{
+	i = input;
+	input = delay_one.dl	(input, (.25*amount) * sampleRate, amount);
+	input = delay_two.dl	(input, (0.5*amount) * sampleRate, amount);	
+	input = delay_three.dl	(input, (0.75*amount)* sampleRate, amount);
+	input = delay_four.dl	(input, (0.1*amount) * sampleRate, amount);	
+	return (input*mix)+(i*(1.0f-mix));	
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+inline double	amp_mod		(double input, double amount)	{
+	return input*(0.5*(1+(am.sinewave(amount*8))));
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+double controlSignal = 0.0f;
+double flangedOut = 0.0f;
+double flangeFeedback = 0.1f;
+
+inline double	flange	(double input, double amount)	{
+	controlSignal	= 0.5*(1+flangerCTL.sinewave(0.25*amount));
+	flangeFeedback	= amount > 0.95 ? 0.95 : amount;
+	flangeFeedback	= flangeFeedback < 0.01 ? 0.01 : flangeFeedback;
+	flangedOut		= flanger.dl(input, (882*controlSignal)+10, flangeFeedback);
+	return sqrt(0.5)*(flangedOut+input);
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+double monoOut = 0.0f;
+
+maxiOsc delayMixer;
+maxiOsc flangeFeedbackMod;
+
 #pragma mark USER RENDERING METHOD
 float*	output	()	{
-    render_output[0]= 0.0f;
-    render_output[1]= 0.0f;
+    render_output[0] = render_output[1] = monoOut = 0.0f;
 	
-	trigger_index+=triggerIncrement;
-	if (trigger_index>480.0f)	{
-		trigger_index = 0.0;
+	if ((int)metro.phasor(metroSpeed)>=1.0)	{
+		metroCount++;
+		if (metroCount>480)
+			metroCount = 0;
+		readTriggers(metroCount);
 	}
-	readTriggers(floorf(trigger_index));
 	
-	render_output[0] = render_output [1] = sample.playOnce(sampleSpeed);
+	if (testVol>0.0f)
+		testVol-=0.0001;
+
+	monoOut = testVol*sample.playOnce(sampleSpeed);
+	monoOut = bitcrusher(monoOut, 0.2);
+	monoOut = distortion(monoOut, 0.5);
+	monoOut = amp_mod(monoOut, 0.1);
+	monoOut = flange(monoOut, 0.5*(1+flangeFeedbackMod.sinewave(0.55)));
+	monoOut = delay(monoOut, 0.5*(1+delayMixer.sinewave(0.5)), 0.5);
+
+	render_output	[0] = monoOut;
+	render_output	[1] = monoOut;
 	
     return render_output;
 }
@@ -153,7 +270,6 @@ OSStatus renderAudioOutput  (
 ////////////////////////////////////////////////////////////
 
 -(void)updatedParameters:(NSMutableArray*)parameters	{
-//	NSLog(@"Parameters received by audio controller: ");	
 	initLocalVariables();
 	
 	NSArray*	points_w = [NSArray arrayWithArray:[parameters objectAtIndex:0]];
@@ -162,32 +278,24 @@ OSStatus renderAudioOutput  (
 	NSArray*	points_z = [NSArray arrayWithArray:[parameters objectAtIndex:3]];	
 	
 	for (NSPoint* p in points_w)	{
-//		NSLog(@"W trigger at %f with pitch %f", p.x, 80.0f-p.y);
-		w_pitches[(int)p.x] = 80.0f-p.y;
+		w_pitches[(int)p.x] = (int)floorf((80.0f-p.y)/8);
 		w_triggers[(int)p.x] = 1.0f;
 	}
-//	NSLog(@"\n\n");	
 	
 	for (NSPoint* p in points_x)	{
-//		NSLog(@"X trigger at %f with pitch %f", p.x, 160.0f-p.y);
-		x_pitches[(int)p.x] = 160.0f-p.y;
+		x_pitches[(int)p.x] = (int)floorf((160.0f-p.y)/8);
 		x_triggers[(int)p.x] = 1.0f;
 	}
-//	NSLog(@"\n\n");
 	
 	for (NSPoint* p in points_y)	{
-//		NSLog(@"Y trigger at %f with pitch %f", p.x, 240.0f-p.y);
-		y_pitches[(int)p.x] = 240.0f-p.y;
+		y_pitches[(int)p.x] = (int)floorf((240.0f-p.y)/8);
 		y_triggers[(int)p.x] = 1.0f;
 	}
-//	NSLog(@"\n\n");
 	
 	for (NSPoint* p in points_z)	{
-//		NSLog(@"Z trigger at %f with pitch %f", p.x, 320.0f-p.y);
-		z_pitches[(int)p.x] = 320.0f-p.y;
+		z_pitches[(int)p.x] = (int)floorf((320.0f-p.y)/8);
 		z_triggers[(int)p.x] = 1.0f;
 	}
-//	NSLog(@"\n\n");
 }
 
 
